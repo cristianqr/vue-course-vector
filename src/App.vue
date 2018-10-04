f1575ca9ef205525fba092707b81e93531ef18b7
Cristian quispe <cristianqr@outlook.com>
2018-10-03 19:15:36 -0500

    Class 06

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,27 @@
 <template>
-  <div class="container">
-    <DynamicStyle></DynamicStyle>
-  </div>
+    <div class="container">
+        <UserNew></UserNew>
+    </div>
 </template>
 
 <script>
-import BindEvent from './components/BindEvent.vue';
-import ComputedProperty from './components/computed-property';
-import LifeCycle from './components/Lifecycle';
-import FirstForm from './components/FirstForm';
-import UserList from './components/UserList';
-import RawHtml from './components/RawHtml';
-import DynamicClass from './components/DynamicClass';
-import DynamicStyle from './components/DynamicStyle';
+    import UserNew from './components/UserNew';
 
-export default {
-  name: 'app',
-  components: {
-      BindEvent,
-      ComputedProperty,
-      UserList,
-      RawHtml,
-      DynamicClass,
-      DynamicStyle
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            UserNew
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/UserNew.vue
+++ src/components/UserNew.vue
@@ -1,42 +1,79 @@
 <template>
-    <div>
+    <form @submit.prevent="saveUser">
         <div class="form-group">
-            <label for="">Nombres</label>
-            <input type="text" class="form-control" :value="user.firstName">
+            <label>Nombres</label>
+            <input type="text" class="form-control" v-model="user.firstName">
+        </div>
+
+        <h2>Genero</h2>
+        <div class="form-group">
+            <div class="form-check">
+                <input type="radio" class="form-check-input" id="female" name="gender" value="F" v-model="user.gender">
+                <label for="female" class="form-check-label">Femenino</label>
+            </div>
+            <div class="form-check">
+                <input type="radio" class="form-check-input" id="male" name="gender" value="M" v-model="user.gender">
+                <label for="male" class="form-check-label">Masculino</label>
+            </div>
         </div>
 
         <div class="form-group">
-            <label for="">Apellido Paterno</label>
-            <input type="text" class="form-control" :value="user.firstName">
+            <select class="form-control" v-model="user.country">
+                <option value="">-- Seleccione Pais --</option>
+                <option :value="country" v-for="country in countryList">{{country}}</option>
+            </select>
         </div>
 
+        <h2>Cursos de interes</h2>
         <div class="form-group">
-            <label for="">Apellido Materno</label>
-            <input type="text" class="form-control" :value="user.lastSurname">
+            <div class="form-check form-check-inline">
+                <input type="checkbox" class="form-check-input" id="angular" value="Angular" v-model="user.courses">
+                <label for="angular" class="form-check-label">Angular</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input type="checkbox" class="form-check-input" id="vue" value="Vue" v-model="user.courses">
+                <label for="vue" class="form-check-label">Vue</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input type="checkbox" class="form-check-input" id="react" value="React" v-model="user.courses">
+                <label for="react" class="form-check-label">React</label>
+            </div>
         </div>
 
-        <div class="form-group">
-            <label for="">Edad</label>
-            <input type="text" class="form-control" :value="user.age">
+        <div class="form-check">
+            <input type="checkbox" class="form-check-input" id="allowSendMail" :true-value="1" :false-value="0" v-model="user.allowSendEmail">
+            <label for="allowSendMail" class="form-check-label">Deseas recibir email?</label>
         </div>
+        <br><br>
 
-        <button  class="btn btn-primary" @click="register">Registrar</button>
-    </div>
+        <button class="btn btn-primary">Registrar</button>
+    </form>
 </template>
 
 <script>
     export default {
-        props: {
-            user: Object
+        data(){
+            return {
+                user: {
+                    firstName: 'Cristian',
+                    gender: 'M',
+                    country: 'PERU',
+                    allowSendEmail: 1,
+                    courses: []
+                },
+                countryList: [
+                    'PERU',
+                    'ARGENTINA',
+                    'COLOMBIA'
+                ]
+            }
         },
         methods: {
-            register(){
-                this.$emit('register');
+            saveUser(){
+                console.log(this.user);
             }
         }
     }
 </script>
-
-<style scoped>
-
-</style>
