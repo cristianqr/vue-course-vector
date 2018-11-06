77f5e1ade97988329c50ce7fdca9f0fe7b63ea61
Cristian quispe <cristianqr@outlook.com>
2018-11-05 19:00:17 -0500

    Class 13-03

---
+++ src/core/http-client.js
@@ -0,0 +1,19 @@
+import Axios from 'axios';
+import {sharedBus} from './sharedBus.js';
+
+
+export const httpClient = Axios.create({
+    baseURL: 'http://localhost:3000'
+});
+
+httpClient.interceptors.request.use((config) => {
+    sharedBus.$emit('ajax-loader:show');
+    return config;
+});
+
+httpClient.interceptors.response.use((response) => {
+    setTimeout(() => {
+        sharedBus.$emit('ajax-loader:hide');
+    }, 1000);
+    return response;
+});
